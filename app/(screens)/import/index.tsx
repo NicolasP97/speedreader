fab0066a570eee42d9cb4e54c3f3300966340bc2
Nicolas <nicolaspaulik97@gmail.com>
2026-01-11 09:55:36 +0100

    App Ordnerstruktur

---
+++ app/(screens)/import/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Import() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Import Screen.</Text>
+    </View>
+  );
+}
