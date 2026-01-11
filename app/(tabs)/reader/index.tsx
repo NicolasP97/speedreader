0d59d73c88f28713762e11a54824b07db68bfc9c
Nicolas <nicolaspaulik97@gmail.com>
2026-01-11 11:00:41 +0100

    Minimaler Reader Screen erstellt

--- app/(tabs)/reader/index.tsx
+++ app/(tabs)/reader/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { AppText } from "../../../components/ui/AppText";
+import { colors } from "../../../constants/colors";
 
-export default function Reader() {
+export default function ReaderScreen() {
+  // Platzhalter – später aus ReaderEngine
+  const currentWord = "Reading";
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Reader Screen.</Text>
+    <View style={styles.container}>
+      <AppText style={styles.word}>{currentWord}</AppText>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  word: {
+    fontSize: 48,
+    fontWeight: "600",
+  },
+});
